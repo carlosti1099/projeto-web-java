f5d2ec460433f5d02e41e2f2ad6ce127bada7a34
carlosti1099 <carlos.ti1099@gmail.com>
2022-05-26 17:46:08 -0400

    Projeto inicial java

---
+++ projetoweb/src/main/java/com/engenheironet/projetoweb/ProjetoWebJavaApplication.java
@@ -0,0 +1,13 @@
+package com.engenheironet.projetoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebJavaApplication.class, args);
+	}
+
+}
---
+++ projetoweb/src/test/java/com/engenheironet/projetoweb/ProjetoWebJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engenheironet.projetoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
